492cd1d9d235976bc9786960a3288e736f948b26
Alex Nelson <alex@postprojects.com>
2025-10-15 10:56:58 -0700

    update: clean config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ['styled-jsx'],
-  },
-  transpilePackages: ['styled-jsx'],
+
 };
 
 export default nextConfig;
